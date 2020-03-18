c25c25c4d52d494b910f7233dfc09eec18fe3edf
Rohit Parulkar <rohit.parulkar@gmail.com>
2020-03-18 11:53:40 -0400

    working rapper and corgi extension

--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -1,6 +1,30 @@
 let images = document.getElementsByTagName('img');
-for(let i = 0; i < images.length; i++){
-  chrome.runtime.sendMessage({msg: 'image', index: i}, function({data, index}){
-    images[index].src = data;
-  });
+corgiImages = [];
+rapperImages = [];
+
+for (let k = 0; k < images.length; k++){
+  Math.random() > 0.3 ? corgiImages.push(images[k]) : rapperImages.push(images[k])
 }
+
+
+chrome.runtime.sendMessage({msg: 'getCorgiPhotosStatic'}, function(data) {
+  let photos = data['data'];
+  for(let i = 0; i < corgiImages.length; i++){
+    const photoURL = photos[Math.floor(Math.random() * photos.length)]
+    corgiImages[i].src = photoURL;
+    corgiImages[i]['data-src'] = photoURL;
+    corgiImages[i]['data-default-src'] = photoURL;
+    corgiImages[i]['srcset'] = photoURL;
+  }
+});
+
+chrome.runtime.sendMessage({msg: 'getRapperPhotos'}, function(data) {
+  let photos = data['data'];
+  for(let i = 0; i < rapperImages.length; i++){
+    const photoURL = photos[Math.floor(Math.random() * photos.length)]
+    rapperImages[i].src = photoURL;
+    rapperImages[i]['data-src'] = photoURL;
+    rapperImages[i]['data-default-src'] = photoURL;
+    rapperImages[i]['srcset'] = photoURL;
+  }
+});
